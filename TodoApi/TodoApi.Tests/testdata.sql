88fec8805921dad93cc8837824bc69d088802e8c
William Lawrence <williamjlawrence1@gmail.com>
2019-05-12 15:45:13 -0400

    Update testdata.sql

--- TodoApi/TodoApi.Tests/testdata.sql
+++ TodoApi/TodoApi.Tests/testdata.sql
@@ -6,4 +6,13 @@ SET IDENTITY_INSERT TodoItems ON;
 INSERT INTO TodoItems (Id, TodoText, IsCompleted, IsDeleted)
 VALUES (1, 'test1', 0, 0);
 
+INSERT INTO TodoItems (Id, TodoText, IsCompleted, IsDeleted)
+VALUES (2, 'test2', 0, 1);
+
+INSERT INTO TodoItems (Id, TodoText, IsCompleted, IsDeleted)
+VALUES (3, 'test3', 1, 0);
+
+INSERT INTO TodoItems (Id, TodoText, IsCompleted, IsDeleted)
+VALUES (4, 'test4', 1, 1);
+
 SET IDENTITY_INSERT TodoItems OFF;
